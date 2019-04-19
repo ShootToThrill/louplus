f6cb6b29c2528193d88bdac3fd93597731b7abb5
tongchuan <tongchuan6666@gmail.com>
2019-04-19 15:58:04 +0800

    加入社保费用计算 计算税后工资

--- calculator.py
+++ calculator.py
@@ -1,35 +1,51 @@
 import sys
 
-base = 5000
+def calculate(data):
+	base = 5000
+	if data.count(':') != 1 or data.startswith(':') or data.endswith(':'):
+		return 'Parameter Error'
 
-if len(sys.argv) != 2:
-	print('Parameter Error')
-	raise ValueError('Parameter Error')
+	code, salary = data.split(':')
 
-salary = sys.argv[1]
+	try:
+		salary_num = int(salary)
+	except ValueError:
+		return 'Parameter Error'
+		
+	insurance_num = insurance(salary_num)
+	taxable = salary_num - base - insurance_num
+	tax = 0
 
-try:
-	salary_num = int(salary)
-except ValueError:
-	print('Parameter Error')
-	exit()
+	if 0 < taxable <= 3000:
+		tax = taxable * 3e-2 - 0
+	elif 3000 < taxable <= 12000:
+		tax = taxable * 10e-2 - 210
+	elif 12000 < taxable <= 25000:
+		tax = taxable * 20e-2 -1410
+	elif 25000 < taxable <= 35000:
+		tax = taxable * 25e-2 - 2660
+	elif 35000 < taxable <= 55000:
+		tax = taxable * 30e-2 - 4410
+	elif 55000 < taxable <= 80000:
+		tax = taxable * 35e-2 -7160
+	elif taxable > 80000:
+		tax = taxable * 40e-2 - 15160
 
-taxable = salary_num - base
-tax = 0
+	return (code,salary_num - tax - insurance_num)
+	
 
-if 0 < taxable <= 3000:
-	tax = taxable * 3e-2 - 0
-elif 3000 < taxable <= 12000:
-	tax = taxable * 10e-2 - 210
-elif 12000 < taxable <= 25000:
-	tax = taxable * 20e-2 -1410
-elif 25000 < taxable <= 35000:
-	tax = taxable * 25e-2 - 2660
-elif 35000 < taxable <= 55000:
-	tax = taxable * 30e-2 - 4410
-elif 55000 < taxable <= 80000:
-	tax = taxable * 35e-2 -7160
-elif taxable > 80000:
-	tax = taxable * 40e-2 - 15160
+def insurance(salary):
+	return  salary * (8+2+0.5+6)/100
 
-print('{:.2f}'.format(tax))
+
+if __name__ == '__main__':
+
+	datas = sys.argv[1:]
+	for data in datas:
+		ret = calculate(data)
+		if isinstance(ret, str):
+			print(ret)
+			continue
+		(code, rel_salary) = calculate(data)
+		print('{}:{:.2f}'.format(code, rel_salary))
+
